b2b2e89ae1872256bb83d3f54285cc16ff7f2c5a
Ian McIntyre <ianpmcintyre@gmail.com>
2021-02-26 07:44:24 -0500

    Prototype usb_device::bus::UsbBus

---
+++ src/bus.rs
@@ -0,0 +1,344 @@
+//! USB bus implementation
+
+use crate::USB;
+use core::cell::RefCell;
+use cortex_m::interrupt::{self, Mutex};
+use imxrt_ral as ral;
+use usb_device::{
+    bus::{PollResult, UsbBus},
+    endpoint::{EndpointAddress, EndpointType},
+    UsbDirection,
+};
+
+pub struct Bus {
+    usb: Mutex<RefCell<USB>>,
+    endpoint_assigner: EndpointAssigner,
+}
+
+fn index(ep_addr: EndpointAddress) -> usize {
+    (ep_addr.index() * 2) + (UsbDirection::In == ep_addr.direction()) as usize
+}
+
+impl Bus {
+    /// Create a USB bus adapter from a `USB` object
+    ///
+    /// Make sure you've fully configured your USB device before wrapping it in `Bus`.
+    ///
+    /// # Safety
+    ///
+    /// The endpoint memory described by the `memory` and `size` pair must outlive the `Bus`.
+    pub unsafe fn new(usb: USB) -> Self {
+        Bus {
+            usb: Mutex::new(RefCell::new(usb)),
+            endpoint_assigner: EndpointAssigner::new(),
+        }
+    }
+    /// Interrupt-safe, immutable access to the USB peripheral
+    fn with_usb<R>(&self, func: impl FnOnce(&USB) -> R) -> R {
+        interrupt::free(|cs| {
+            let usb = self.usb.borrow(cs);
+            let usb = usb.borrow();
+            func(&*usb)
+        })
+    }
+    /// Interrupt-safe, mutable access to the USB peripheral
+    fn with_usb_mut<R>(&self, func: impl FnOnce(&mut USB) -> R) -> R {
+        interrupt::free(|cs| {
+            let usb = self.usb.borrow(cs);
+            let mut usb = usb.borrow_mut();
+            func(&mut *usb)
+        })
+    }
+
+    pub fn configure_endpoints(&self) {
+        self.with_usb_mut(|usb| {
+            for ep in usb.endpoints.iter_mut().flat_map(core::convert::identity) {
+                ep.configure(&usb.usb);
+                if UsbDirection::Out == ep.address().direction() {
+                    let max_packet_len = ep.max_packet_len();
+                    ep.schedule_transfer(&usb.usb, max_packet_len);
+                }
+            }
+        });
+    }
+}
+
+unsafe impl Send for crate::buffer::Allocator {}
+unsafe impl Send for crate::Endpoint {}
+
+impl UsbBus for Bus {
+    /// The USB hardware can guarantee that we set the status before we receive
+    /// the status, and we're taking advantage of that. We expect this flag to
+    /// result in a call to set_address before the status happens. This means
+    /// that we can meet the timing requirements without help from software.
+    ///
+    /// It's not a quirk; it's a feature :)
+    const QUIRK_SET_ADDRESS_BEFORE_STATUS: bool = true;
+
+    fn alloc_ep(
+        &mut self,
+        ep_dir: UsbDirection,
+        ep_addr: Option<EndpointAddress>,
+        ep_type: EndpointType,
+        max_packet_size: u16,
+        _interval: u8,
+    ) -> usb_device::Result<EndpointAddress> {
+        let ep_addr = if let Some(ep_addr) = ep_addr {
+            if !self
+                .endpoint_assigner
+                .assign_endpoint(ep_addr.index(), ep_addr.direction())
+            {
+                return Err(usb_device::UsbError::InvalidEndpoint);
+            }
+            ep_addr
+        } else if let Some(ep_idx) = self.endpoint_assigner.assign_next_endpoint(ep_dir) {
+            EndpointAddress::from_parts(ep_idx as usize, ep_dir)
+        } else {
+            return Err(usb_device::UsbError::EndpointOverflow);
+        };
+
+        self.with_usb_mut(|usb| {
+            let buffer = match usb.buffer_allocator.allocate(max_packet_size as usize) {
+                Some(buffer) => buffer,
+                None => return Err(usb_device::UsbError::BufferOverflow),
+            };
+
+            let qh: &'static crate::qh::QH = &usb.qhs[index(ep_addr)];
+            let td: &'static crate::td::TD = &usb.tds[index(ep_addr)];
+
+            qh.set_max_packet_len(max_packet_size as usize);
+            qh.set_zero_length_termination(false);
+            qh.set_interrupt_on_setup(
+                ep_type == EndpointType::Control && ep_addr.direction() == UsbDirection::Out,
+            );
+
+            td.set_terminate();
+            td.clear_status();
+
+            let mut ep: crate::Endpoint = unsafe {
+                match ep_type {
+                    EndpointType::Control => crate::endpoint::control(ep_addr, qh, td, buffer),
+                    EndpointType::Bulk => crate::endpoint::bulk(ep_addr, qh, td, buffer),
+                    EndpointType::Interrupt => crate::endpoint::interrupt(ep_addr, qh, td, buffer),
+                    EndpointType::Isochronous => {
+                        unimplemented!("No support for isochronous endpoints")
+                    }
+                }
+            };
+            ep.initialize(&usb.usb);
+
+            usb.endpoints[index(ep_addr)] = Some(ep);
+            Ok(ep_addr)
+        })
+    }
+
+    fn set_device_address(&self, addr: u8) {
+        self.with_usb_mut(|usb| usb.set_address(addr));
+    }
+
+    fn enable(&mut self) {
+        self.with_usb_mut(|usb| usb.attach());
+    }
+
+    fn reset(&self) {
+        self.with_usb_mut(|usb| {
+            usb.bus_reset();
+
+            let ctrl0_out: EndpointAddress = EndpointAddress::from_parts(0, UsbDirection::Out);
+            if let Some(ep) = &mut usb.endpoints[index(ctrl0_out)] {
+                let size = ep.max_packet_len();
+                ep.schedule_transfer(&usb.usb, size);
+            } else {
+                panic!("No control OUT endpoint allocated");
+            }
+        });
+    }
+
+    fn write(&self, ep_addr: EndpointAddress, buf: &[u8]) -> usb_device::Result<usize> {
+        self.with_usb_mut(|usb| {
+            let ep = match &mut usb.endpoints[index(ep_addr)] {
+                Some(ref mut ep) => ep,
+                None => return Err(usb_device::UsbError::InvalidEndpoint),
+            };
+
+            if ep.is_transfer_error() || ep.is_transfer_halted() {
+                return Err(usb_device::UsbError::InvalidState);
+            } else if ep.is_transfer_active() {
+                return Err(usb_device::UsbError::WouldBlock);
+            }
+
+            ep.clear_nack(&usb.usb);
+
+            let written = ep.write(buf);
+            ep.schedule_transfer(&usb.usb, written);
+
+            Ok(written)
+        })
+    }
+
+    fn read(&self, ep_addr: EndpointAddress, buf: &mut [u8]) -> usb_device::Result<usize> {
+        self.with_usb_mut(|usb| {
+            let ep = match &mut usb.endpoints[index(ep_addr)] {
+                Some(ref mut ep) => ep,
+                None => return Err(usb_device::UsbError::InvalidEndpoint),
+            };
+
+            if ep.is_transfer_error() || ep.is_transfer_halted() {
+                return Err(usb_device::UsbError::InvalidState);
+            } else if ep.is_transfer_active() {
+                return Err(usb_device::UsbError::WouldBlock);
+            }
+
+            if ep_addr.index() == 0 {
+                // Do they want to read the setup data? Let's guess...
+                if ep.has_setup(&usb.usb) && buf.len() >= 8 {
+                    let setup = ep.read_setup(&usb.usb);
+                    buf[..8].copy_from_slice(&setup.to_le_bytes());
+                    return Ok(8);
+                }
+            }
+
+            ep.clear_complete(&usb.usb);
+            ep.clear_nack(&usb.usb);
+
+            let read = ep.read(buf);
+            let max_packet_len = ep.max_packet_len();
+            ep.schedule_transfer(&usb.usb, max_packet_len);
+
+            Ok(read)
+        })
+    }
+
+    fn set_stalled(&self, ep_addr: EndpointAddress, stalled: bool) {
+        self.with_usb_mut(|usb| {
+            if let Some(ep) = &mut usb.endpoints[index(ep_addr)] {
+                ep.set_stalled(&usb.usb, stalled);
+            }
+        });
+    }
+
+    fn is_stalled(&self, ep_addr: EndpointAddress) -> bool {
+        self.with_usb(|usb| {
+            if let Some(ep) = &usb.endpoints[index(ep_addr)] {
+                ep.is_stalled(&usb.usb)
+            } else {
+                panic!("is_stalled called on an invalid endpoint")
+            }
+        })
+    }
+
+    fn suspend(&self) {
+        unimplemented!("Nothing to do; not signaling suspend / resume from poll")
+    }
+
+    fn resume(&self) {
+        unimplemented!("Nothing to do; not signaling suspend / resume from poll")
+    }
+
+    fn poll(&self) -> PollResult {
+        self.with_usb_mut(|usb| {
+            let usbsts = ral::read_reg!(ral::usb, usb.usb, USBSTS);
+            ral::write_reg!(ral::usb, usb.usb, USBSTS, usbsts);
+
+            use ral::usb::USBSTS;
+            if usbsts & USBSTS::URI::mask != 0 {
+                PollResult::Reset
+            } else if usbsts & USBSTS::UI::mask != 0 {
+                // Note: could be complete in one register read, but this is a little
+                // easier to read...
+                let ep_out = ral::read_reg!(ral::usb, usb.usb, ENDPTCOMPLETE, ERCE);
+
+                let ep_in_complete = ral::read_reg!(ral::usb, usb.usb, ENDPTCOMPLETE, ETCE);
+                ral::write_reg!(ral::usb, usb.usb, ENDPTCOMPLETE, ETCE: ep_in_complete);
+
+                let ep_setup = ral::read_reg!(ral::usb, usb.usb, ENDPTSETUPSTAT) as u16;
+
+                PollResult::Data {
+                    ep_out: ep_out as u16,
+                    ep_in_complete: ep_in_complete as u16,
+                    ep_setup,
+                }
+            } else {
+                PollResult::None
+            }
+        })
+    }
+}
+
+struct EndpointAssigner {
+    ep_out: u16,
+    ep_in: u16,
+}
+
+impl EndpointAssigner {
+    const fn new() -> Self {
+        EndpointAssigner {
+            ep_out: 0,
+            ep_in: 0,
+        }
+    }
+    /// Assigns a new endpoint (never EP0)
+    fn assign_next_endpoint(&mut self, direction: UsbDirection) -> Option<u32> {
+        let mask: &mut u16 = match direction {
+            UsbDirection::Out => &mut self.ep_out,
+            UsbDirection::In => &mut self.ep_in,
+        };
+        let offset = (*mask | 1).trailing_ones();
+        if offset == 8 {
+            None
+        } else {
+            *mask |= 1 << offset;
+            Some(offset)
+        }
+    }
+    /// Assigns a specific endpoint (the only way to assign EP0)
+    fn assign_endpoint(&mut self, endpoint: usize, direction: UsbDirection) -> bool {
+        let mask: &mut u16 = match direction {
+            UsbDirection::Out => &mut self.ep_out,
+            UsbDirection::In => &mut self.ep_in,
+        };
+        if *mask & (1 << endpoint) != 0 {
+            false
+        } else {
+            *mask |= 1 << endpoint;
+            true
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::EndpointAssigner;
+    use super::UsbDirection;
+    #[test]
+    fn endpoint_next_assignment() {
+        let mut eps = EndpointAssigner::new();
+        for idx in 1..8 {
+            assert_eq!(eps.assign_next_endpoint(UsbDirection::Out), Some(idx));
+        }
+
+        assert_eq!(eps.assign_next_endpoint(UsbDirection::Out), None);
+
+        for idx in 1..8 {
+            assert_eq!(eps.assign_next_endpoint(UsbDirection::In), Some(idx));
+        }
+
+        assert_eq!(eps.assign_next_endpoint(UsbDirection::In), None);
+    }
+
+    #[test]
+    fn endpoint_assignment() {
+        let mut eps = EndpointAssigner::new();
+        assert!(eps.assign_endpoint(3, UsbDirection::Out));
+        for idx in 1..3 {
+            assert_eq!(eps.assign_next_endpoint(UsbDirection::Out), Some(idx));
+        }
+        assert!(!eps.assign_endpoint(2, UsbDirection::Out));
+        for idx in 4..8 {
+            assert_eq!(eps.assign_next_endpoint(UsbDirection::Out), Some(idx));
+        }
+        assert_eq!(eps.assign_next_endpoint(UsbDirection::Out), None);
+        assert!(eps.assign_endpoint(0, UsbDirection::Out));
+        assert!(!eps.assign_endpoint(0, UsbDirection::Out));
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,86 @@
 #![no_std]
 
 mod buffer;
+mod bus;
+mod endpoint;
 mod pll;
 mod qh;
 mod td;
 mod vcell;
 
+pub use bus::Bus;
+
 use imxrt_ral as ral;
+
+type Endpoint = endpoint::Endpoint<'static>;
+
+const EP_INIT: [Option<Endpoint>; QH_COUNT] = [
+    None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+];
+
+pub struct USB {
+    endpoints: [Option<Endpoint>; QH_COUNT],
+    usb: ral::usb::Instance,
+    phy: ral::usbphy::Instance,
+    qhs: &'static [qh::QH; QH_COUNT],
+    tds: &'static [td::TD; QH_COUNT],
+    buffer_allocator: buffer::Allocator,
+}
+
+impl USB {
+    pub fn new(usb: ral::usb::Instance, phy: ral::usbphy::Instance) -> Self {
+        // Safety: taking static memory. Assumes that the provided
+        // USB instance is a singleton, which is the only safe way for it
+        // to exist.
+        match (&*usb as *const _, &*phy as *const _) {
+            (ral::usb::USB1, ral::usbphy::USBPHY1) => USB {
+                usb,
+                phy,
+                endpoints: EP_INIT,
+                qhs: &USB1_STATE.qhs.0,
+                tds: &USB1_STATE.tds.0,
+                buffer_allocator: buffer::Allocator::empty(),
+            },
+            (ral::usb::USB2, ral::usbphy::USBPHY2) => USB {
+                usb,
+                phy,
+                endpoints: EP_INIT,
+                qhs: &USB2_STATE.qhs.0,
+                tds: &USB2_STATE.tds.0,
+                buffer_allocator: buffer::Allocator::empty(),
+            },
+            _ => panic!("Mismatch USB and USBPHY"),
+        }
+    }
+
+    /// Set the region of memory that can be used for transfers with endpoints
+    ///
+    /// # Safety
+    ///
+    /// Caller must ensure that `memory` is valid for the `size` bytes. Caller must ensure that
+    /// the allocation isn't used anywhere else.
+    pub unsafe fn set_endpoint_memory(&mut self, memory: core::ptr::NonNull<u8>, size: usize) {
+        self.buffer_allocator = buffer::Allocator::new(memory, size);
+    }
+
+    pub fn initialize(&mut self, ccm_analog: &ral::ccm_analog::Instance) {
+        pll::initialize(ccm_analog);
+        initialize(&self.usb, &self.phy, ccm_analog);
+        set_enpoint_list_address(&self.usb, self.qhs.as_ptr() as *const _);
+    }
+
+    fn set_address(&mut self, address: u8) {
+        set_address(&self.usb, address);
+    }
+
+    fn attach(&mut self) {
+        attach(&self.usb);
+    }
+
+    fn bus_reset(&mut self) {
+        bus_reset(&self.usb);
+    }
+}
 
 //
 // Helpers
@@ -65,3 +139,78 @@ fn bus_reset(usb: &ral::usb::Instance) {
         "Took too long to handle bus reset"
     );
 }
+
+//
+// Static memory
+//
+
+/// Eight endpoints, two directions
+const QH_COUNT: usize = 8 * 2;
+
+/// A list of transfer descriptors
+///
+/// Supports 1 TD per QH (per endpoint direction)
+#[repr(align(32))]
+struct TDList([td::TD; QH_COUNT]);
+const TD_LIST_INIT: TDList = TDList([
+    td::TD::new(),
+    td::TD::new(),
+    td::TD::new(),
+    td::TD::new(),
+    td::TD::new(),
+    td::TD::new(),
+    td::TD::new(),
+    td::TD::new(),
+    td::TD::new(),
+    td::TD::new(),
+    td::TD::new(),
+    td::TD::new(),
+    td::TD::new(),
+    td::TD::new(),
+    td::TD::new(),
+    td::TD::new(),
+]);
+
+/// A list of queue heads
+///
+/// One queue head per endpoint, per direction (default).
+#[repr(align(4096))]
+struct QHList([qh::QH; QH_COUNT]);
+const QH_LIST_INIT: QHList = QHList([
+    qh::QH::new(),
+    qh::QH::new(),
+    qh::QH::new(),
+    qh::QH::new(),
+    qh::QH::new(),
+    qh::QH::new(),
+    qh::QH::new(),
+    qh::QH::new(),
+    qh::QH::new(),
+    qh::QH::new(),
+    qh::QH::new(),
+    qh::QH::new(),
+    qh::QH::new(),
+    qh::QH::new(),
+    qh::QH::new(),
+    qh::QH::new(),
+]);
+
+/// USB driver state
+struct State {
+    qhs: QHList,
+    tds: TDList,
+}
+
+const STATE_INIT: State = State {
+    qhs: QH_LIST_INIT,
+    tds: TD_LIST_INIT,
+};
+
+static USB1_STATE: State = STATE_INIT;
+static USB2_STATE: State = STATE_INIT;
+
+unsafe impl Send for qh::QH {}
+unsafe impl Send for td::TD {}
+
+unsafe impl Sync for qh::QH {}
+unsafe impl Sync for td::TD {}
