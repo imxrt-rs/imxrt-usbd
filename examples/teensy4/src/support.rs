e8f5206426b74967c483209da77355b6ffb57925
Ian McIntyre <ianpmcintyre@gmail.com>
2021-02-28 09:10:22 -0500

    Remove PLL configuration

--- examples/teensy4/src/support.rs
+++ examples/teensy4/src/support.rs
@@ -35,37 +35,3 @@ pub fn new_bus_adapter() -> imxrt_usbd::usb1::BusAdapter {
         imxrt_usbd::usb1::BusAdapter::new(usb, usbphy, &mut ENDPOINT_MEMORY)
     }
 }
-
-pub mod ccm {
-    use super::ral;
-
-    /// Initialize CCM clocks for USB1
-    pub fn initialize(ccm: &ral::ccm::Instance, ccm_analog: &ral::ccm_analog::Instance) {
-        // Enable the PLL...
-        loop {
-            if ral::read_reg!(ral::ccm_analog, ccm_analog, PLL_USB1, ENABLE == 0) {
-                ral::write_reg!(ral::ccm_analog, ccm_analog, PLL_USB1_SET, ENABLE: 1);
-                continue;
-            }
-            if ral::read_reg!(ral::ccm_analog, ccm_analog, PLL_USB1, POWER == 0) {
-                ral::write_reg!(ral::ccm_analog, ccm_analog, PLL_USB1_SET, POWER: 1);
-                continue;
-            }
-            if ral::read_reg!(ral::ccm_analog, ccm_analog, PLL_USB1, LOCK == 0) {
-                continue;
-            }
-            if ral::read_reg!(ral::ccm_analog, ccm_analog, PLL_USB1, BYPASS == 1) {
-                ral::write_reg!(ral::ccm_analog, ccm_analog, PLL_USB1_CLR, BYPASS: 1);
-                continue;
-            }
-            if ral::read_reg!(ral::ccm_analog, ccm_analog, PLL_USB1, EN_USB_CLKS == 0) {
-                ral::write_reg!(ral::ccm_analog, ccm_analog, PLL_USB1_SET, EN_USB_CLKS: 1);
-                continue;
-            }
-            break;
-        }
-
-        // Enable the clock gates...
-        ral::modify_reg!(ral::ccm, ccm, CCGR6, CG1: 0b11, CG0: 0b11);
-    }
-}
